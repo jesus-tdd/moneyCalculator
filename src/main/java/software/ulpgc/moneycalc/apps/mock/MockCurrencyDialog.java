6c66c698120f56d33ae5b408c06ebfb620542fa1
Jesus <jesus.tdd03@gmail.com>
2025-01-10 13:13:16 +0000

    Created class MockCurrencyDialog

---
+++ src/main/java/software/ulpgc/moneycalc/apps/mock/MockCurrencyDialog.java
@@ -0,0 +1,19 @@
+package software.ulpgc.moneycalc.apps.mock;
+
+import software.ulpgc.moneycalc.architecture.view.CurrencyDialog;
+import software.ulpgc.moneycalc.architecture.model.Currency;
+
+import java.util.List;
+
+public class MockCurrencyDialog implements CurrencyDialog {
+    private final List<Currency> currencies;
+
+    public MockCurrencyDialog(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    @Override
+    public software.ulpgc.moneycalc.architecture.model.Currency get() {
+        return currencies.getFirst();
+    }
+}
