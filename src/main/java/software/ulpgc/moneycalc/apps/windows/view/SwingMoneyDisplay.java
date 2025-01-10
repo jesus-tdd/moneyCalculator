3bfaee567f4eb478ed1230cc02817a5ef1795b5b
Jesus <jesus.tdd03@gmail.com>
2025-01-10 13:51:07 +0000

    Implemented class SwingMoneyDisplay

--- src/main/java/software/ulpgc/moneycalc/apps/windows/view/SwingMoneyDisplay.java
+++ src/main/java/software/ulpgc/moneycalc/apps/windows/view/SwingMoneyDisplay.java
@@ -1,4 +1,23 @@
 package software.ulpgc.moneycalc.apps.windows.view;
 
-public class SwingMoneyDisplay {
+import software.ulpgc.moneycalc.architecture.model.Money;
+import software.ulpgc.moneycalc.architecture.view.MoneyDisplay;
+
+import javax.swing.*;
+
+public class SwingMoneyDisplay extends JPanel implements MoneyDisplay {
+    private final JLabel label;
+
+    public SwingMoneyDisplay() {
+        this.add(label = createLabel());
+    }
+
+    private JLabel createLabel() {
+        return new JLabel();
+    }
+
+    @Override
+    public void show(Money money) {
+        label.setText(money.amount()+ " " + money.currency().code());
+    }
 }
