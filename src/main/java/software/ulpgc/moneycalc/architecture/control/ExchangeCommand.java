84fa0cc06b90394a0513b55e707fff20449ede78
Jesus <jesus.tdd03@gmail.com>
2025-01-10 12:16:01 +0000

    Fixed minor issues

--- src/main/java/software/ulpgc/moneycalc/architecture/control/ExchangeCommand.java
+++ src/main/java/software/ulpgc/moneycalc/architecture/control/ExchangeCommand.java
@@ -1,6 +1,6 @@
 package software.ulpgc.moneycalc.architecture.control;
 
-public class ExchangeCommand implements Command{
+public class ExchangeCommand implements Command {
     @Override
     public void execute() {
 
