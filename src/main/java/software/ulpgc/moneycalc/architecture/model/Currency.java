f2089341cb652a40faa05acf25bdb0a2377e34f7
Jesus <jesus.tdd03@gmail.com>
2025-01-10 14:15:09 +0000

    Added toString()

--- src/main/java/software/ulpgc/moneycalc/architecture/model/Currency.java
+++ src/main/java/software/ulpgc/moneycalc/architecture/model/Currency.java
@@ -1,4 +1,8 @@
 package software.ulpgc.moneycalc.architecture.model;
 
 public record Currency(String name, String code, String symbol) {
+    @Override
+    public String toString() {
+        return code;
+    }
 }
