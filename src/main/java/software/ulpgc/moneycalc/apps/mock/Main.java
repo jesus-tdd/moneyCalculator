154257bd13c804e1a37c287a372dc7c1b7e1fdcf
Jesus <jesus.tdd03@gmail.com>
2025-01-10 13:14:59 +0000

    Implemented Mock app

--- src/main/java/software/ulpgc/moneycalc/apps/mock/Main.java
+++ src/main/java/software/ulpgc/moneycalc/apps/mock/Main.java
@@ -1,6 +1,30 @@
 package software.ulpgc.moneycalc.apps.mock;
 
+import software.ulpgc.moneycalc.architecture.control.Command;
+import software.ulpgc.moneycalc.architecture.control.ExchangeCommand;
+import software.ulpgc.moneycalc.architecture.model.Currency;
+import software.ulpgc.moneycalc.architecture.persistence.ExchangeRateLoader;
+import software.ulpgc.moneycalc.architecture.view.CurrencyDialog;
+import software.ulpgc.moneycalc.architecture.view.MoneyDialog;
+import software.ulpgc.moneycalc.architecture.view.MoneyDisplay;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+        List<Currency> currencies = new MockCurrencyLoader().load();
+        MoneyDialog moneyDialog = new MockMoneyDialog(currencies);
+        CurrencyDialog currencyDialog = new MockCurrencyDialog(currencies);
+        ExchangeRateLoader loader = new MockExchangeRateLoader();
+        MoneyDisplay moneyDisplay = new MockMoneyDisplay();
+        Command command = new ExchangeCommand(
+                moneyDialog,
+                currencyDialog,
+                moneyDisplay,
+                loader
+        );
+        command.execute();
+    }
 }
