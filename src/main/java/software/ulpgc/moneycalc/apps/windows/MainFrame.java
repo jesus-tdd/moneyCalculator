76e24d3d94930e5bc783e10f5087b7df0292561e
Jesus <jesus.tdd03@gmail.com>
2025-01-10 14:05:34 +0000

    Implemented class MainFrame

---
+++ src/main/java/software/ulpgc/moneycalc/apps/windows/MainFrame.java
@@ -0,0 +1,73 @@
+package software.ulpgc.moneycalc.apps.windows;
+
+import software.ulpgc.moneycalc.apps.windows.view.SwingCurrencyDialog;
+import software.ulpgc.moneycalc.apps.windows.view.SwingMoneyDialog;
+import software.ulpgc.moneycalc.apps.windows.view.SwingMoneyDisplay;
+import software.ulpgc.moneycalc.architecture.control.Command;
+import software.ulpgc.moneycalc.architecture.model.Currency;
+
+import javax.swing.*;
+import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainFrame extends JFrame {
+    private final List<Currency> currencies;
+    private final SwingMoneyDialog moneyDialog;
+    private final SwingCurrencyDialog currencyDialog;
+    private final SwingMoneyDisplay moneyDisplay;
+    private final Map<String, Command> commands;
+
+    public MainFrame(List<Currency> currencies) {
+        this.currencies = currencies;
+        this.setTitle("MoneyCalculator");
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setLayout(new FlowLayout());
+        this.add(moneyDialog = createMoneyDialog());
+        this.add(currencyDialog = createCurrencyDialog());
+        this.add(moneyDisplay = createMoneyDisplay());
+        this.add(createCalculateButton());
+        this.commands = new HashMap<>();
+    }
+
+    private SwingMoneyDisplay createMoneyDisplay() {
+        return new SwingMoneyDisplay();
+    }
+
+    private SwingCurrencyDialog createCurrencyDialog() {
+        return new SwingCurrencyDialog(currencies);
+    }
+
+    private SwingMoneyDialog createMoneyDialog() {
+        return new SwingMoneyDialog(currencies);
+    }
+
+    private Component createCalculateButton() {
+        JButton button = new JButton("Calculate");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get("exchange").execute();
+            }
+        });
+        return button;
+    }
+
+    public SwingMoneyDialog getMoneyDialog() {
+        return moneyDialog;
+    }
+
+    public SwingCurrencyDialog getCurrencyDialog() {
+        return currencyDialog;
+    }
+
+    public SwingMoneyDisplay getMoneyDisplay() {
+        return moneyDisplay;
+    }
+}
