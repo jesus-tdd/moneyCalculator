839b9abb341c7009973b674353c340be4633fded
Jesus <jesus.tdd03@gmail.com>
2025-01-10 14:09:31 +0000

    Implemented windows main

--- src/main/java/software/ulpgc/moneycalc/apps/windows/Main.java
+++ src/main/java/software/ulpgc/moneycalc/apps/windows/Main.java
@@ -1,4 +1,23 @@
 package software.ulpgc.moneycalc.apps.windows;
 
+import software.ulpgc.moneycalc.apps.mock.MockCurrencyLoader;
+import software.ulpgc.moneycalc.apps.mock.MockExchangeRateLoader;
+import software.ulpgc.moneycalc.architecture.control.ExchangeCommand;
+import software.ulpgc.moneycalc.architecture.model.Currency;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Currency> currencies = new MockCurrencyLoader().load();
+        MainFrame mainFrame = new MainFrame(currencies);
+        ExchangeCommand exchangeCommand = new ExchangeCommand(
+                mainFrame.getMoneyDialog(),
+                mainFrame.getCurrencyDialog(),
+                new MockExchangeRateLoader(),
+                mainFrame.getMoneyDisplay()
+        );
+        mainFrame.add("exchange", exchangeCommand);
+        mainFrame.setVisible(true);
+    }
 }
