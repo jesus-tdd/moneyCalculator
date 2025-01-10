13fc20e8f3162fa2f11b8df77c9b760e4df141dc
Jesus <jesus.tdd03@gmail.com>
2025-01-10 13:58:05 +0000

    Implemented class SwingCurrencyDialog

--- src/main/java/software/ulpgc/moneycalc/apps/windows/view/SwingCurrencyDialog.java
+++ src/main/java/software/ulpgc/moneycalc/apps/windows/view/SwingCurrencyDialog.java
@@ -1,4 +1,31 @@
 package software.ulpgc.moneycalc.apps.windows.view;
 
-public class SwingCurrencyDialog {
+import software.ulpgc.moneycalc.architecture.model.Currency;
+import software.ulpgc.moneycalc.architecture.view.CurrencyDialog;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class SwingCurrencyDialog extends JPanel implements CurrencyDialog {
+    private final List<Currency> currencies;
+    private final JComboBox<Currency> selector;
+
+    public SwingCurrencyDialog(List<Currency> currencies) {
+        this.currencies = currencies;
+        this.add(selector = createSelector());
+    }
+
+    private JComboBox<Currency> createSelector() {
+        JComboBox<Currency> comboBox = new JComboBox<>();
+        for (Currency currency : currencies) {
+            comboBox.addItem(currency);
+        }
+        return comboBox;
+    }
+
+    @Override
+    public Currency get() {
+        return currencies.get(selector.getSelectedIndex());
+    }
 }
