9945a4e49696c041cfdbfdfa5c3cbfe5458015f5
Jesus <jesus.tdd03@gmail.com>
2025-01-11 12:36:49 +0000

    Updated implemented APICurrencyLoader to main

--- src/main/java/software/ulpgc/moneycalc/apps/windows/Main.java
+++ src/main/java/software/ulpgc/moneycalc/apps/windows/Main.java
@@ -2,6 +2,8 @@ package software.ulpgc.moneycalc.apps.windows;
 
 import software.ulpgc.moneycalc.apps.mock.MockCurrencyLoader;
 import software.ulpgc.moneycalc.apps.mock.MockExchangeRateLoader;
+import software.ulpgc.moneycalc.apps.windows.persistence.APICurrencyLoader;
+import software.ulpgc.moneycalc.apps.windows.persistence.ExchangeRatesAPI;
 import software.ulpgc.moneycalc.architecture.control.ExchangeCommand;
 import software.ulpgc.moneycalc.architecture.model.Currency;
 
@@ -9,7 +11,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Currency> currencies = new MockCurrencyLoader().load();
+        ExchangeRatesAPI api = new ExchangeRatesAPI("d32be90457df4aa5ae81d9546dfb8a20");
+        List<Currency> currencies = new APICurrencyLoader(api).load();
         MainFrame mainFrame = new MainFrame(currencies);
         ExchangeCommand exchangeCommand = new ExchangeCommand(
                 mainFrame.getMoneyDialog(),
